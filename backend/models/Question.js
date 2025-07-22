272074e8fffe1bc6cb188d8f251e823c679ad38c
Anushka Dilshan <anushkadilshan990@gmail.com>
2025-07-22 14:53:04 +0530

    add  Question.js model

---
+++ backend/models/Question.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+
+    exam_id: { 
+        type: mongoose.Schema.Types.ObjectId, ref: 'Exam', 
+        required: true 
+    },
+    question_text: { 
+        type: String, 
+        required: true 
+    },
+    options: [{ 
+        type: String, 
+        required: true 
+    }],
+    correct_option: { 
+        type: String, 
+        required: true 
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Question', questionSchema);
