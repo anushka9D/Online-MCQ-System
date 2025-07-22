cc4f1e5c5a8f2af993c11f291e20ca14876f42fb
Anushka Dilshan <anushkadilshan990@gmail.com>
2025-07-22 14:22:59 +0530

    add needed dependencies and setup index.js file

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Route
+
+//Database Connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("mongoo db url connected");
+        app.listen(process.env.PORT);
+        console.log(`app running in port ${process.env.PORT}`);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
